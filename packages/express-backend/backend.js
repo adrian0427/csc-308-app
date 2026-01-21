a0a4dd99c5f7284287046c926c5f663b2b00c76b
adrian0427 <adrian.val0427@outlook.com>
2026-01-20 20:19:42 -0800

    Feat: Added POST /users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -44,6 +44,11 @@ const users = {
   ],
 };
 
+const addUser = (user) => {
+  users.users_list.push(user);
+  return user;
+};
+
 const findUserByName = (name) => {
   return users["users_list"].filter((user) => user["name"] === name);
 };
@@ -70,4 +75,11 @@ app.get("/users/:id", (req, res) => {
   } else {
     res.json(result);
   }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+
+  res.send();
 });
